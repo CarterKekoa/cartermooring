789537870b02aa387fcf3eeabd95acfe66ee2214
CarterKekoa <carterkekoa@gmail.com>
2021-01-04 18:02:49 -0800

    Basic personal website made

---
+++ website.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, url_for, redirect, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def mainPage():
+    return render_template('main.html')
+
+
+if __name__ == "__main__":      #if running from command line, turn on dev mode
+    app.run(debug=True)         #dev mode, server updates on own, shows errors
